0470704bd5019495575215af7c79efeb17086fda
plate katai5 <katai5plate.sub@gmail.com>
2019-11-13 19:01:04 +0900

    apply mykit-core

--- index.js
+++ index.js
@@ -1,37 +1,3 @@
-const fs = require("fs-extra");
 const resources = require(`${__dirname}/resources.json`);
 const scripts = require(`${__dirname}/scripts.json`);
-module.exports = async () => {
-  resources.forEach(({ src, dir, dist }) => {
-    const root = !!dir ? `./${dir}/` : "./";
-    const dest = `${root}${!!dist ? dist : src}`;
-    const read = `${__dirname}/resources/${src}`;
-    try {
-      fs.outputFileSync(dest, fs.readFileSync(read));
-      console.log(`ADDED ${dest}`);
-    } catch (e) {
-      console.log(`FAILED ${dest}`, e, { src, dir, dist, root, dest, read });
-      process.exit(1);
-    }
-  });
-  const pkg = "./package.json";
-  try {
-    if (fs.existsSync(pkg)) {
-      console.log(`DETECTED ${pkg}`);
-      const currentPkg = fs.readJSONSync(pkg);
-      currentPkg.scripts = { ...(currentPkg.scripts || {}), ...scripts };
-      fs.outputFileSync(pkg, JSON.stringify(currentPkg, void 0, "  "));
-      console.log(`ADDED SCRIPTS IN ${pkg}`);
-    } else {
-      console.log(`${pkg} IS NOT FOUND`);
-      fs.outputFileSync(pkg, JSON.stringify({ scripts }, void 0, "  "));
-      console.log(`ADDED ${pkg}`);
-    }
-  } catch (e) {
-    console.log(`FAILED ${pkg}`, e, { pkg, scripts });
-    process.exit(1);
-  }
-  console.log("DONE");
-  console.log("");
-  console.log("RUN THIS:\n\nyarn mykit-install");
-};
+module.exports = () => require("mykit-core")(resources, scripts);
